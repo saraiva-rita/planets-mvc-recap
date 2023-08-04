0c3ccd3e8718d8e56825c08c4512a5b3fb33c045
Rita Saraiva <rita_saraiva@hotmail.com>
2023-08-04 10:12:58 +0100

    solved recap

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
 // Require Packages
-const mongoose = require("mongoose");
-const express = require("express");
-const hbs = require ("hbs");
+const mongoose = require('mongoose');
+const express = require('express');
+const hbs = require('hbs');
 
 // Require Model
 const Planet = require('./models/Planet.js');
 
 // Import Data from JS File
-let planets = require("./utils/planets.js"); 
+let planets = require('./utils/planets.js');
 
 // Mongoose Configuration
-mongoose.connect('mongodb://127.0.0.1/planets-recap')
-.then(info => console.log('Database Connected'))
-.catch(err => console.error('Error', err));
+mongoose
+  .connect('mongodb://127.0.0.1/planets-recap')
+  .then((info) => console.log('Database Connected'))
+  .catch((err) => console.error('Error', err));
 
 // Create an Express Server that handles requests and responses
 const app = express();
 
 // Set HBS as View Engine
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 
 // Point to Views Directory
-app.set("views", __dirname + "/views");
+app.set('views', __dirname + '/views');
 
 // Make Public Static Files Available
 app.use(express.static('public'));
 
 // Home Page with Index
-app.get("/", async (req,res)=>{
-    res.render("index");
+app.get('/', async (req, res) => {
+  res.render('index');
 });
 
 // Button to Get Planets - DO NOT TOUCH THIS ROUTE
-app.post("/create-planets", async (req,res)=>{
-   for (let i = 0; i < planets.length; i++){
-    const {name} = planets[i];
-    let verifyPlanet = await Planet.find({name: name}); 
-    if(!verifyPlanet.length){
-        Planet.create(planets[i]);  
+app.post('/create-planets', async (req, res) => {
+  for (let i = 0; i < planets.length; i++) {
+    const { name } = planets[i];
+    let verifyPlanet = await Planet.find({ name: name });
+    if (!verifyPlanet.length) {
+      Planet.create(planets[i]);
     }
-   }
-   res.redirect('/');
+  }
+  res.redirect('/');
 });
 
 // Button to Delete All Planets - DO NOT TOUCH THIS ROUTE
-app.post("/delete-planets", async (req,res)=>{
-    await Planet.deleteMany({});
-    res.redirect('/');
-
+app.post('/delete-planets', async (req, res) => {
+  await Planet.deleteMany({});
+  res.redirect('/');
 });
 
 // Planets List Page - Your Code Goes inside this Route
-app.get("/list-planets", async (req,res)=>{
-    res.render('list-planets'); 
+app.get('/list-planets', async (req, res) => {
+  let showPlanet = await Planet.find();
+  res.render('list-planets', { showPlanet });
 });
-
 
 // Listen in PORT 3000
 app.listen(3000);
--- models/Planet.js
+++ models/Planet.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const planetSchema = new Schema({
-    /* Your Code Goes Here ... */
-}); 
+  /* Your Code Goes Here ... */
+  name: { type: String },
+  image: { type: String },
+  velocity: { type: Number },
+  distance: { type: Number },
+  description: { type: String },
+});
 
 const Planet = mongoose.model('Planet', planetSchema);
 
